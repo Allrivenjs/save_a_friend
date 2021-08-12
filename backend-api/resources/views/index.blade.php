e9fbb82cec4cd5fa8cfdf169be6c939c74edb6f6
Allrivenjs <jaimeruizxdd2@gmail.com>
2021-08-12 15:48:37 -0500

    Install Tailwindcss
    uwu

--- backend-api/resources/views/index.blade.php
+++ backend-api/resources/views/index.blade.php
@@ -5,9 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Save a friend</title>
+        <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     </head>
    <body>
     <div id="root"></div>
     <script src="{{mix('/js/app.js')}}"></script>
+
    </body>
 </html>
