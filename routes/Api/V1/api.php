480508b3f920b10c922db742d6fe574d476273bf
Allrivenjs <jaimeruizxdd2@gmail.com>
2021-08-31 00:52:33 -0500

    medios cambios
    medios cambios

--- routes/Api/V1/api.php
+++ routes/Api/V1/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\CEOController;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Auth\AuthControllor;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -19,9 +20,9 @@ Route::middleware('logroute')->group(function (){
         Route::apiResource('/profile', ProfileController::class)->except('store');
         Route::get('/profiles', [ProfileController::class, 'getAllProfile']);
         Route::post('logout/',[AuthControllor::class, 'logout']);
-     //   Route::apiResource('/ceo',  CEOController::class );
-     //   Route::apiResource('/post',  PostController::class)->only('store' , 'show', 'update', 'destroy');
-       // Route::post('/posts', [PostController::class, 'Posts']);
+        Route::apiResource('/user',  UserController::class);
+     // Route::apiResource('/ceo',  CEOController::class );
+    //  Route::post('/posts', [PostController::class, 'Posts']);
     });
 
 
